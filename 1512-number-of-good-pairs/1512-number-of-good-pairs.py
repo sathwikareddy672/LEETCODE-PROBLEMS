7cc739cc03854a3e1c712b99abfac41ed07217cb
Sathwika reddy <sathwikareddy672@gmail.com>
2026-07-22 11:12:49 +0530

    Time: 0 ms (100%), Space: 19.1 MB (97.72%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -3,6 +3,6 @@ class Solution:
         count=0
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-                if nums[i]==nums[j] and i<j:
+                if nums[i]==nums[j] :
                    count+=1
         return count
