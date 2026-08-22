ae6955bf865746483de872f7fb9427e21b17ca80
Sathwika reddy <sathwikareddy672@gmail.com>
2026-08-22 16:28:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (91.31%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -1,9 +1,8 @@
 class Solution:
     def checkDivisibility(self, n: int) -> bool:
         s,m=0,1
-        for j in str(n):
-            s+=int(j)
         for i in str(n):
+            s+=int(i)
             m*=int(i)
         return n%(s+m)==0
         
